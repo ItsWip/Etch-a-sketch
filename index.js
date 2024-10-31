538e10cfd825bffbf91ce569c8448c2d9d80be21
ItsWip <pswaraj0614@gmail.com>
2024-10-31 11:49:36 +0530

    Add function to black, random and erase button

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 document.addEventListener("DOMContentLoaded", function(){
-    create_grids(16);
+    let color= "black";
+    createboard(16);
+
+    let select_btn= document.querySelector("#prompt");
+    select_btn.addEventListener("click",function(){
+        let size=getsize();
+        createboard(size);
+    })
 })
 
-function create_grids(size){
-    let grids= document.querySelector(".grids");
+function createboard(size){
+    let grids=document.querySelector(".grids");
 
-    grids.style.gridTemplateColumns =`repeat(${size},1fr)`;
-    grids.style.gridTemplateRows =`repeat(${size},1fr)`;
+    grids.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
+    grids.style.gridTemplateRows= `repeat(${size}, 1fr)`;
 
-    let total_grids=(size*size);
+    let total_grids= size*size;
 
-    for(let i=0; i<total_grids; i++){
+    for(let i=0;i<total_grids;i++){
         let grid=document.createElement("div");
-        grid.style.backgroundColor="grey";
-        grids.insertAdjacentElement("beforeend",grid);   
+        grid.addEventListener("mouseover", color_selection);
+        grids.insertAdjacentElement("beforeend",grid);
+    }
+}
+
+function getsize(){
+    let input= prompt("Enter size of grids you want.\n Number should be between 0 and hundred");
+    let reply= document.querySelector("#reply");
+
+    if (input === ""){
+        reply.innerHTML= "Don't you have a keyboard?\n Use that education loan to buy one.";
+    }else if(input>0 && input<100){
+        reply.innerHTML= "Just do it."
+        return input;
+    }else{
+        reply.innerHTML= "Last time someone did this was a blind arts student. \n Is it you again?";
+    }
+}
+
+function set_color(new_color){
+    color= new_color;
+    return color;
+}
+
+function color_selection(){
+    if(color=== "random"){
+        this.style.backgroundColor= `hsl(${Math.random()*360},100%,50%)`;
+    }else if(color=== "erase"){
+        this.style.backgroundColor= `#c5c6d0`;
+    }else{
+        this.style.backgroundColor= "black";
     }
 }
